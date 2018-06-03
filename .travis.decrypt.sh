fe747b1c2cdd2af8a20fede5a578f765396ad4aa
Luis de la Garza <delagarza@gmail.com>
2018-06-03 11:21:26 +0200

    Renamed a variable; making sure to use the container-based hosts

--- .travis.decrypt.sh
+++ .travis.decrypt.sh
@@ -20,7 +20,7 @@
 RSA_PRIVATE_KEY_DESTINATION=$1
 
 # location on which we will unencrypt the file
-UNENCRYPTED_FILE_DESTINATION="/tmp/travis_deployment_key"
+DELETE_ME_ALWAYS="/tmp/travis_deployment_key"
 # file separator
 FILE_SEPARATOR="###-#-#-#-quick-and-dirty-file-separator-THOU-SHALL-NOT-PASS-#-#-#-###"
 
@@ -30,27 +30,29 @@ FILE_SEPARATOR="###-#-#-#-quick-and-dirty-file-separator-THOU-SHALL-NOT-PASS-#-#
 ## for every repository, make sure you edit the names accordingly.    ##
 ##                                                                    ##
 ########################################################################          <---DO NOT EDIT PAST THE SHOWN COLUMN, LEST YOU WANT TO OPEN PANDORA'S BOX
-openssl aes-256-cbc -K $encrypted_3f7041caa607_key -iv $encrypted_3f7041caa607_iv -in travis_deployment_key.enc -out $UNENCRYPTED_FILE_DESTINATION -d
+openssl aes-256-cbc -K $encrypted_3f7041caa607_key -iv $encrypted_3f7041caa607_iv -in travis_deployment_key.enc -out $DELETE_ME_ALWAYS -d
 echo "Secret sauce recipe preparation returned: $?"
 
 # find in which line is our file separator
-FILE_SEPARATOR_LINE=`grep --line-number "$FILE_SEPARATOR" $UNENCRYPTED_FILE_DESTINATION | cut --fields 1 --delimiter=:`
+FILE_SEPARATOR_LINE=`grep --line-number "$FILE_SEPARATOR" $DELETE_ME_ALWAYS | cut --fields 1 --delimiter=:`
 
 # we know that the first part contains a script that will set all required environment variables, we just source it as-is
 echo "Sourcing first $(( FILE_SEPARATOR_LINE - 1 )) lines..."
 unset THE_EAGLE_HAS_LANDED
-source <(head --lines $(( $FILE_SEPARATOR_LINE - 1 )) $UNENCRYPTED_FILE_DESTINATION)
+source <(head --lines $(( $FILE_SEPARATOR_LINE - 1 )) $DELETE_ME_ALWAYS)
 # do some "sanity check"
 if [ "$THE_EAGLE_HAS_LANDED" = "wizzard" ]; then
       # the second part of the unencrypted file contains a private key, output the last part of the unencrypted file into the desired destination
-      tail --lines +$(( $FILE_SEPARATOR_LINE + 1 )) $UNENCRYPTED_FILE_DESTINATION > $RSA_PRIVATE_KEY_DESTINATION
+      tail --lines +$(( $FILE_SEPARATOR_LINE + 1 )) $DELETE_ME_ALWAYS > $RSA_PRIVATE_KEY_DESTINATION
       # make sure to change file permissions on this very special file
       chmod 600 $RSA_PRIVATE_KEY_DESTINATION 
+      # remove the unencrypted file
+      rm -f $DELETE_ME_ALWAYS
 else
       (>&2 echo "[DUN GOOFED] I'm afraid I can't do that, Dave. Something went wrong with the deployment key file. Private key will not be extracted and deployment WILL fail.")
       # better safe than sorry
       rm -f RSA_PRIVATE_KEY_DESTINATION
 fi
 
-# remove the unencrypted file
-rm -f $UNENCRYPTED_FILE_DESTINATION
+# remove the unencrypted file (again, maybe)
+rm -f $DELETE_ME_ALWAYS
